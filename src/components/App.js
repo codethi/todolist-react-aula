a1054b5d1c828085dee3aecc71a865a2a39ebbf8
codethi <docencia.thiago@gmail.com>
2022-09-22 17:19:05 -0300

    feat: acelera ai 22-09-22

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,63 @@
-import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
-  const [tarefas, setTarefas] = React.useState([]);
+  const [tasks, setTasks] = useState([]);
 
-  function adicionarTarefa() {
+  function addNewTask() {
     const input = document.querySelector("input");
-    const novaTarefa = input.value;
-    setTarefas([...tarefas, novaTarefa]);
+    const newTask = { id: tasks.length + 1, title: input.value, done: false };
+    setTasks([...tasks, newTask]);
     input.value = "";
   }
 
+  function deleteTask(idTask) {
+    const newArrayTasks = tasks.filter((task) => task.id !== idTask);
+    setTasks(newArrayTasks);
+  }
+
+  function markTaskAsDone(idTask) {
+    tasks.forEach((task) =>
+      task.id === idTask ? (task.done = !task.done) : ""
+    );
+    setTasks([...tasks]);
+  }
+
+  /* function markTaskAsDone(idTask) {
+    tasks.forEach((task) => {
+      if (task.id === idTask) {
+        task.done ? (task.done = false) : (task.done = true);
+      }
+    });
+    setTasks([...tasks]);
+  } */
+
   return (
     <main>
-      <input type="text" placeholder="Digite aqui sua tarefa" />
-      <button onClick={adicionarTarefa}>Adicionar</button>
-      {tarefas.map((tarefa) => (
-        <p>{tarefa}</p>
-      ))}
+      <section className="input-container">
+        <input type="text" placeholder="Digite aqui sua tarefa" />
+        <button onClick={addNewTask}>Adicionar</button>
+      </section>
+
+      <ul className="tarefas-container">
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span
+              onClick={() => markTaskAsDone(task.id)}
+              className={task.done ? "done" : ""}
+            >
+              <ion-icon
+                name={task.done ? "checkmark-circle" : "ellipse-outline"}
+              ></ion-icon>{" "}
+              {task.title}
+            </span>
+            <ion-icon
+              onClick={() => deleteTask(task.id)}
+              name="close-circle-outline"
+            ></ion-icon>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import reactDOM  from "react-dom";
-import App from './components/App'
+import App from './components/App';
+import "./reset.css"
 
 reactDOM.render(<App/>, document.querySelector('.root'))
